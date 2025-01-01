ab94ec0d8ed47a27e6dd775d640f27ee700b4117
Adithya Ekanayaka <adithyasean@outlook.com>
2025-01-01 21:42:29 +0530

    initial commit

---
+++ src/main/java/com/nextstep/recommendations/NextStepRecommendationsApplication.java
@@ -0,0 +1,13 @@
+package com.nextstep.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextStepRecommendationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextStepRecommendationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nextstep/recommendations/NextStepRecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextstep.recommendations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepRecommendationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
