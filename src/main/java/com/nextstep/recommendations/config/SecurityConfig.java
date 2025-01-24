68b329515bac786e8ed3e4aab2aa75bbfc89ea6a
Adithya Ekanayaka <adithyasean@outlook.com>
2025-01-24 18:47:30 +0530

    CorsConfig refactored to new SecurityConfig

--- src/main/java/com/nextstep/recommendations/config/SecurityConfig.java
+++ src/main/java/com/nextstep/recommendations/config/SecurityConfig.java
@@ -1,23 +1,25 @@
 package com.nextstep.recommendations.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class SecurityConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")  // Allow all origins
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
-                        .allowedHeaders("*")  // Allowed headers
-                        .allowCredentials(true);  // Allow credentials
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
