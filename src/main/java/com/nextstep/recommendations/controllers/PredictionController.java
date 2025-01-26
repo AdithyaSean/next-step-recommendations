84e04521dda97baaa87d0bdf990ea6590696cab9
Adithya Ekanayaka <adithyasean@outlook.com>
2025-01-26 08:52:42 +0530

    without lombok

--- src/main/java/com/nextstep/recommendations/controllers/PredictionController.java
+++ src/main/java/com/nextstep/recommendations/controllers/PredictionController.java
@@ -2,15 +2,19 @@ package com.nextstep.recommendations.controllers;
 
 import com.nextstep.recommendations.dto.StudentProfileDTO;
 import com.nextstep.recommendations.service.PredictionService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/predictions")
 public class PredictionController {
 
     private final PredictionService predictionService;
+
+    @Autowired
+    public PredictionController(PredictionService predictionService) {
+        this.predictionService = predictionService;
+    }
 
     @PostMapping("/")
     public StudentProfileDTO updateStudentProfile(@RequestBody StudentProfileDTO studentProfileDTO) throws Exception {
