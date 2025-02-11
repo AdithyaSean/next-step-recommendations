16100547fb23d9469b257e3ddd3db9ad76eb507c
Adithya Ekanayaka <adithyasean@outlook.com>
2025-02-11 23:33:45 +0530

    test

--- src/test/java/com/nextstep/recommendations/src/ModelRegistry.java
+++ src/test/java/com/nextstep/recommendations/src/ModelRegistry.java
@@ -1,38 +1,38 @@
-package com.nextstep.recommendations.src;
-
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-public class ModelRegistry {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private Integer streamId;
-    private int version;
-    private LocalDateTime timestamp;
-    private String modelPath;
-
-    public ModelRegistry() {}
-
-    public ModelRegistry(Integer streamId, int version, LocalDateTime timestamp, String modelPath) {
-        this.streamId = streamId;
-        this.version = version;
-        this.timestamp = timestamp;
-        this.modelPath = modelPath;
-    }
-
-    // Getters
-    public Long getId() { return id; }
-    public Integer getStreamId() { return streamId; }
-    public int getVersion() { return version; }
-    public LocalDateTime getTimestamp() { return timestamp; }
-    public String getModelPath() { return modelPath; }
-
-    // Setters
-    public void setStreamId(Integer streamId) { this.streamId = streamId; }
-    public void setVersion(int version) { this.version = version; }
-    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
-    public void setModelPath(String modelPath) { this.modelPath = modelPath; }
-}
+//package com.nextstep.recommendations.src;
+//
+//import jakarta.persistence.*;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//public class ModelRegistry {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private Integer streamId;
+//    private int version;
+//    private LocalDateTime timestamp;
+//    private String modelPath;
+//
+//    public ModelRegistry() {}
+//
+//    public ModelRegistry(Integer streamId, int version, LocalDateTime timestamp, String modelPath) {
+//        this.streamId = streamId;
+//        this.version = version;
+//        this.timestamp = timestamp;
+//        this.modelPath = modelPath;
+//    }
+//
+//    // Getters
+//    public Long getId() { return id; }
+//    public Integer getStreamId() { return streamId; }
+//    public int getVersion() { return version; }
+//    public LocalDateTime getTimestamp() { return timestamp; }
+//    public String getModelPath() { return modelPath; }
+//
+//    // Setters
+//    public void setStreamId(Integer streamId) { this.streamId = streamId; }
+//    public void setVersion(int version) { this.version = version; }
+//    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+//    public void setModelPath(String modelPath) { this.modelPath = modelPath; }
+//}
--- src/test/java/com/nextstep/recommendations/src/ModelRegistryRepository.java
+++ src/test/java/com/nextstep/recommendations/src/ModelRegistryRepository.java
@@ -1,9 +1,9 @@
-package com.nextstep.recommendations.src;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ModelRegistryRepository extends JpaRepository<ModelRegistry, Long> {
-    Optional<ModelRegistry> findFirstByStreamIdOrderByVersionDesc(Integer streamId);
-    Optional<ModelRegistry> findFirstByStreamIdIsNullOrderByVersionDesc();
-}
+//package com.nextstep.recommendations.src;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import java.util.Optional;
+//
+//public interface ModelRegistryRepository extends JpaRepository<ModelRegistry, Long> {
+//    Optional<ModelRegistry> findFirstByStreamIdOrderByVersionDesc(Integer streamId);
+//    Optional<ModelRegistry> findFirstByStreamIdIsNullOrderByVersionDesc();
+//}
